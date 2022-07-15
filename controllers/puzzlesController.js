84dcd3b65bbceab9703476520f193f062e4a2f6e
Derek Mason <3derekmason@gmail.com>
2022-07-15 09:50:54 -0600

    puzzles schema and re-init

--- controllers/puzzlesController.js
+++
@@ -1,8 +0,0 @@
-const puzzlesModel = require('../models/puzzlesModel');
-
-exports.getAllPuzzles = async(req, res, next) => {
-  try{
-    const puzzlesData = await puzzlesModel.find();
-    res.status(200).send(puzzlesData.json());
-  }
-}
--- routes/router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const puzzlesController = require("../controllers/puzzlesController.js");
-
-router.get("/", puzzlesController.getAllPuzzles);
-
-module.exports = router;
