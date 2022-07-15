46c05d3d58bad7aba91661bedebdf3114f574028
Derek Mason <3derekmason@gmail.com>
2022-07-15 09:23:17 -0600

    server and db connection init

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 
+const port = process.env.PORT || 5000;
+
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((result) => {
+    app.listen(port, () => {
+      console.log(`Connected on port: ${port}`);
+    });
+  })
+  .catch((err) => console.error(err));
