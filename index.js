100cfd066986ea0750ee98526d15b4c8fb9532a0
Derek Mason <3derekmason@gmail.com>
2022-07-15 11:38:16 -0600

    edit server

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = require("./routes/router.js");
 const cors = require("cors");
+const PORT = process.env.PORT || 5000;
 
-// mongodb+srv://derekmason:<password>@saor-blog.rxnmh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 var corsOptions = {
   origin: "*",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -14,7 +14,6 @@ app.use(express.json());
 app.use(cors(corsOptions));
 app.use("/api/", router);
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Connected on port: ${PORT}`);
 });
